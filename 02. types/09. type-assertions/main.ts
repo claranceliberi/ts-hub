b45bc8390ae4873aafea4d10def2ec9f39c6cddf
Ntwari Clarance Liberiste <liberintwari@gmail.com>
2021-07-27 16:52:30 +0200

    docs: some more info on type assertion

--- 02. types/09. type-assertions/main.ts
+++ 02. types/09. type-assertions/main.ts
@@ -9,4 +9,13 @@ const x = "hello" as number;
 // Conversion of type 'string' to type 'number' may be a mistake because neither type sufficiently overlaps with the other. If this was intentional, convert the expression to 'unknown' first.
 
 
+// so there might be time where you will face this error 
+// and you have no other way or you are just seriously 
+// about what you are doing.
+// Or may be it might be someones code you don't want to mess ub with 
+// so you can easily do as the error sugest
+
+const y = "hello" as unknown as number; // this tricky ts compiler and it will just be like normal js, but actually it is a string
+
+
 // const a = (expr as any) as T;
